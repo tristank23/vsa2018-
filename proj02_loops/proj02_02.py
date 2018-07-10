610bb0bbe8f695bd259faf76bcb8955c3e19b757
tristank23 <40799393+tristank23@users.noreply.github.com>
2018-07-10 10:23:16 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -10,4 +10,32 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 
+# key word is for
+
+# loop over a range of #s
+#
+# for num in range (51):
+#     sum = sum + num
+#     print sum
+#
+# s = "vsa"
+# #loop over strings
+# for letter in s:
+#     print letter
+#     if letter == "s":
+#         print "This is an s"
+
+stop = raw_input("How many Fibonacci numbers would you lke to generate? ")
+stop = int(stop)
+num = 0
+prev_num = 0
+current_num = 1
+
+
+for num in range(stop):
+    print current_num
+    next_num = current_num + prev_num
+    prev_num = current_num
+    current_num = next_num
+    num = num + 1
 
