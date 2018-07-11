1114e4cdb49634786654e8e1367ce91b9881c42b
tristank23 <40799393+tristank23@users.noreply.github.com>
2018-07-11 11:21:11 -0500

    Add files via upload

--- proj04_lists/proj04.py
+++ proj04_lists/proj04.py
@@ -15,6 +15,53 @@ practice with lists
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #and write a program that prints out all the elements of the list that are less than 5.
 
+# var = ["a", "b", "cat", 3]
+# #index
+# print var[0]
+# #slice of list
+# print var[0:3]
+# #all but first
+# print var[1:]
+# #all but last
+# print var[:-1]
+# #replace
+# var[0] = "tree"
+# print var
+# #loop
+# #to change
+# counter = 0
+# for item in var:
+#     if item == "cat":
+#         var[counter] = "dog"
+#     elif item == "tree":
+#         var[counter] = "flower"
+#     counter = counter + 1
+# print var
+# #to add on the end of a list
+# var.append(28)
+# print var
+# #to split a string into a list
+# lst = []
+# s = "This is a string"
+# for letter in s:
+#     lst.append(letter)
+# print lst
+#
+# new_lst = []
+# for item in a:
+#     if item < 5:
+#         print item
+#         new_lst.append(item)
+#
+# print new_lst
+# compare = raw_input("Enter a number: ")
+# compare = int(compare)
+# new_lst2 = []
+# for item in a:
+#     if item < compare:
+#         new_lst2.append(item)
+# print new_lst2
+
 
 
 
@@ -27,6 +74,13 @@ c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # and write a program that creates and prints a list that contains only the elements
 # that are common between the lists (without duplicates).
 # Make sure your program works on two lists of different sizes.
+# lst = []
+# for item in a:
+#     if item in b:
+#         if item not in lst:
+#             lst.append(item)
+# print lst
+
 
 
 
@@ -37,6 +91,14 @@ c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
 # and write a program that replaces all “a” with “*”.
+# counter = 0
+# for item in d:
+#     if item == "a":
+#         d[counter] = "*"
+#     counter = counter + 1
+# print d
+
+
 
 
 
@@ -48,6 +110,27 @@ d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
 
 #Part IV
 #Ask the user for a string, and print out whether this string is a palindrome or not.
+
+s = raw_input("Enter a word or expression: ")
+s_show = s
+s = s[0:].lower()
+lst = []
+counter = 0
+for letter in s:
+    lst.append(letter)
+for item in lst:
+    if item == " ":
+        lst.remove(item)
+for item in range(len(lst)):
+    if len(lst) == 0:
+        break
+    elif lst[0] == lst[-1]:
+        lst = lst[1:-1]
+    counter = counter + 1
+if len(lst) <= 1:
+    print s_show + " is a palindrome"
+else:
+    print s_show + " is not a palindrome"
 
 
 
